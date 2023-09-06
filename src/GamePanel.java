e6b467ae118d553e36f1a151ad1d4a38ba807d99
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-06 15:20:15 -0700

    added key pressed events

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -135,7 +135,28 @@ public class GamePanel extends JPanel implements ActionListener {
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT -> {
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                }
+                case KeyEvent.VK_RIGHT -> {
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                }
+                case KeyEvent.VK_UP -> {
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                }
+                case KeyEvent.VK_DOWN -> {
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                }
+            }
         }
     }
 }
